859b598e9c2e75b98c7e0afb158c815fb485dfee
AmigoDroid <colteman05@gmail.com>
2021-09-16 19:46:33 -0300

    function sobre add

--- javascript/splash.js
+++ javascript/splash.js
@@ -6,3 +6,9 @@ function entrar(){
 function cadastrar(){
     window.location.assign(url_cadastrar);
 }
+
+var sobre =document.getElementById('sobre');
+sobre.addEventListener('click',function(){
+    var msg ="Olá, os seus dados são armazenados na sua maquina (localStorage). Para ver os dados click com o botão direito do mause / inspecionar procure a opção Application se seu navegador for o google Chrome."
+    alert(msg)
+});

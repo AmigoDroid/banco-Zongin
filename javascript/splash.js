4bfa1a8dc6c8277f285c01d3a9c1b207dc65088c
AmigoDroid <colteman05@gmail.com>
2021-09-15 21:49:01 -0300

    script splash

---
+++ javascript/splash.js
@@ -0,0 +1,8 @@
+var url_login='/pages/login.html'
+var url_cadastrar='/pages/cadastrar.html'
+function entrar(){
+    window.location.assign(url_login);
+}
+function cadastrar(){
+    window.location.assign(url_cadastrar);
+}

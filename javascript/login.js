911f87f73b43c68f0871a4f7bb85eb7f1a563ec2
AmigoDroid <colteman05@gmail.com>
2021-09-15 22:50:13 -0300

    login js

---
+++ javascript/login.js
@@ -0,0 +1,20 @@
+var login ={
+    usuario:localStorage.getItem('usuario'),
+    senha:localStorage.getItem('senha')
+}
+var forme = document.getElementById('formulario');
+forme.addEventListener('submit',function(e){
+
+    e.preventDefault(); 
+    let us = document.getElementById('usuario').value;
+    let pas = document.getElementById('senha').value;
+    if(login.usuario===null || login.senha===null){
+        alert("você precisa se cadastrar!")
+    }else if(us===''|| pas===''){
+        console.log('preencha os campos');
+    }else if(us==login.usuario && pas===login.senha){
+        console.log('logado!');
+    }else{
+        alert('usuario ou senha incorretos!')
+    }
+});

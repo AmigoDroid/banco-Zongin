417c4c63d5aae4a3ecc2edde2841d737712d1db8
AmigoDroid <colteman05@gmail.com>
2021-09-15 22:25:09 -0300

    js cadastrar local storage

---
+++ javascript/cadastrar.js
@@ -0,0 +1,59 @@
+
+var url_login='login.html'
+var user={
+    nome:'',
+    sobrenome:'',
+    cpf:'',
+    usuario:'',
+    senha:'',
+    cadastrar(){
+        localStorage.setItem('nome',user.nome);
+        localStorage.setItem('sobrenome',user.sobrenome);
+        localStorage.setItem('cpf',user.cpf);
+        localStorage.setItem('usuario',user.usuario);
+        localStorage.setItem('senha',user.senha);
+    },
+    apagar(){
+        localStorage.clear();
+    }
+}
+var forme = document.getElementById('formulario');
+forme.addEventListener('submit',function(e){
+    e.preventDefault();
+})
+function cadastrar(){
+let nm = document.getElementById('nome').value;
+let snm = document.getElementById('sobrenome').value;
+let cp = document.getElementById('cpf').value;
+let us = document.getElementById('usuario').value;
+let sn = document.getElementById('senha').value;
+if(
+    nm==='' ||
+    snm==='' ||
+    cp==='' ||
+    us==='' ||
+    sn===''
+    ){
+        console.log('algum campo vazio!');
+    }else{
+        console.log('tudo certo!');
+        user.nome=nm;
+        user.sobrenome=snm;
+        user.cpf=cp;
+        user.usuario=us;
+        user.senha=sn;
+        //
+        inserir(user);
+    }
+}
+function inserir(obj){
+    var id_cpf = window.localStorage.getItem('cpf')
+    if(id_cpf===obj.cpf){
+        alert('esse CPF já foi cadastrado!');
+    }else{
+          obj.cadastrar();
+  window.location.replace(url_login)
+    }
+
+
+}

5d7587c54f379c3468cef8bbc6e05f47aa574d90
AmigoDroid <coltman05@gmail.com>
2021-09-19 20:57:14 -0300

    teste

--- javascript/login/autenticador.js
+++ javascript/login/autenticador.js
@@ -13,7 +13,7 @@ function auteticar(us,pas){
         break;
       }else{
         if (i>=users.length-1) {
-          console.log('ERRO');
+          ///console.log('ERRO');
           alert('usuario ou senha incorretos');
           break;
         }
